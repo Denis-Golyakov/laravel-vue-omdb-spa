e25ca56d17dfe64f3b9a636dc0067ec9e99c7b27
Denis Golyakov <denis@golyakov.com>
2026-05-14 18:17:11 +0300

    chore: add docblocks to omdb service

--- app/app/Services/OmdbService.php
+++ app/app/Services/OmdbService.php
@@ -5,6 +5,11 @@ namespace App\Services;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
+/**
+ * OmdbService class.
+ *
+ * This class provides methods for interacting with the OMDB API.
+ */
 class OmdbService
 {
     private readonly string $apiKey;
@@ -18,11 +23,25 @@ class OmdbService
         $this->apiRoute = config('services.omdb.route');
     }
 
+    /**
+     * Generate a cache key for the given type and value.
+     *
+     * @param string $type The type of the cache key.
+     * @param string $value The value to generate the cache key from.
+     * @return string The generated cache key.
+     */
     private function getCacheKey(string $type, string $value): string
     {
         return \sprintf("omdb.%s.%s", $type, md5($value));
     }
 
+    /**
+     * Fetch data from the OMDB API and cache it.
+     *
+     * @param string $cacheKey The cache key.
+     * @param array $parameters The request parameters.
+     * @return array The fetched data.
+     */
     private function fetch(string $cacheKey, array $parameters): array
     {
         return Cache::remember($cacheKey, self::CACHE_TTL, function () use ($parameters) {
@@ -40,6 +59,12 @@ class OmdbService
         });
     }
 
+    /**
+     * Find movie details by IMDb ID.
+     *
+     * @param string $imdbId The IMDb ID of the movie.
+     * @return array The movie details.
+     */
     public function find(string $imdbId): array
     {
         return $this->fetch(
@@ -51,6 +76,12 @@ class OmdbService
         );
     }
 
+    /**
+     * Search for movies by query.
+     *
+     * @param string $searchQuery The search query.
+     * @return array The search results.
+     */
     public function search(string $searchQuery): array
     {
         return $this->fetch(
